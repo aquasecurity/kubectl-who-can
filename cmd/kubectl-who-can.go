57e4e393b55a7555c2f34b5ffde6ce72c0aeebb7
Daniel Pacak <pacak.daniel@gmail.com>
2019-06-26 10:25:05 +0200

    docs: Update README.md

--- cmd/kubectl-who-can.go
+++ cmd/kubectl-who-can.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/aquasecurity/kubectl-who-can/pkg/cmd"
 	clioptions "k8s.io/cli-runtime/pkg/genericclioptions"
+	// Load all known auth plugins
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	"os"
 )
