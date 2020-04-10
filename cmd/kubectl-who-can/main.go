62cefc634b7458342011f94c626d1c48f59cbbe4
Athanasios Kostopoulos <athanasios@akostopoulos.com>
2020-04-10 16:02:09 +0200

    fixed parsing of flags

--- cmd/kubectl-who-can/main.go
+++ cmd/kubectl-who-can/main.go
@@ -5,11 +5,29 @@ import (
 	"github.com/aquasecurity/kubectl-who-can/pkg/cmd"
 	clioptions "k8s.io/cli-runtime/pkg/genericclioptions"
 	// Load all known auth plugins
+	"flag"
+	"github.com/spf13/pflag"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/klog"
 	"os"
 )
 
+func initFlags() {
+	klog.InitFlags(nil)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+
+	// Hide all klog flags except for -v
+	flag.CommandLine.VisitAll(func(f *flag.Flag) {
+		if f.Name != "v" {
+			pflag.Lookup(f.Name).Hidden = true
+		}
+	})
+}
+
 func main() {
+	defer klog.Flush()
+
+	initFlags()
 	root, err := cmd.NewWhoCanCommand(clioptions.IOStreams{In: os.Stdin, Out: os.Stdout, ErrOut: os.Stderr})
 	if err != nil {
 		fmt.Printf("Error: %v\n", err)
