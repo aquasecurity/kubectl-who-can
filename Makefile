36b47915a100054bb014a63cdeff7c9aac885db8
Daniel Pacak <pacak.daniel@gmail.com>
2020-06-26 15:36:24 +0200

    refactor: Use KIND to run integration tests (#77)
    Signed-off-by: Daniel Pacak <pacak.daniel@gmail.com>

--- Makefile
+++ Makefile
@@ -6,10 +6,8 @@ build: kubectl-who-can
 $(BINARY): $(SOURCES)
 	GO111MODULE=on CGO_ENABLED=0 go build -o $(BINARY) ./cmd/kubectl-who-can/main.go
 
-tests: $(SOURCES)
+unit-tests: $(SOURCES)
 	GO111MODULE=on go test -v -short -race -timeout 30s -coverprofile=coverage.txt -covermode=atomic ./...
 
 integration-tests: $(SOURCES)
-	GO111MODULE=on GOOS=linux CGO_ENABLED=0 go test -v -c -o test/bin/integration_test test/integration_test.go
-	@docker-compose -f test/docker-compose.yaml build --no-cache --force-rm
-	@docker-compose -f test/docker-compose.yaml up --abort-on-container-exit --exit-code-from test
+	GO111MODULE=on go test -v test/integration_test.go
--- test/integration_test.go
+++ test/integration_test.go
@@ -2,6 +2,12 @@ package test
 
 import (
 	"bytes"
+	"os"
+	"strings"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/labels"
+
 	"github.com/aquasecurity/kubectl-who-can/pkg/cmd"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -13,19 +19,22 @@ import (
 	clioptions "k8s.io/cli-runtime/pkg/genericclioptions"
 	client "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
-	"os"
-	"strings"
-	"testing"
-	"time"
+)
+
+var (
+	// commonLabels is a set of common labels added to each object created by this integration test, which allows us
+	// to do a proper cleanup and distinguish them from the default object created on cluster init.
+	//
+	// kubectl delete ns,crd,role,rolebinding,clusterrole,clusterrolebindings -l app.kubernetes.io/name=who-can
+	commonLabels = labels.Set{
+		"app.kubernetes.io/name": "who-can",
+	}
 )
 
 func TestIntegration(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Integration test")
 	}
-
-	// TODO Wait for KUBECONFIG
-	time.Sleep(10 * time.Second)
 
 	config, err := clientcmd.BuildConfigFromFlags("", os.Getenv("KUBECONFIG"))
 	require.NoError(t, err)
@@ -39,14 +48,14 @@ func TestIntegration(t *testing.T) {
 	createCRDs(t, extClient.CustomResourceDefinitions())
 	configureRBAC(t, coreClient)
 
-	data := []struct {
-		scenario string
-		args     []string
-		output   []string
+	testCases := []struct {
+		name   string
+		args   []string
+		output []string
 	}{
 		{
-			scenario: "Should print who can create configmaps",
-			args:     []string{"create", "cm"},
+			name: "Should print who can create configmaps",
+			args: []string{"create", "cm"},
 			output: []string{
 				"ROLEBINDING                  NAMESPACE  SUBJECT  TYPE  SA-NAMESPACE",
 				"alice-can-create-configmaps  default    Alice    User",
@@ -54,43 +63,44 @@ func TestIntegration(t *testing.T) {
 			},
 		},
 		{
-			scenario: "Should print who can get /healthz",
-			args:     []string{"get", "/logs"},
+			name: "Should print who can get /healthz",
+			args: []string{"get", "/logs"},
 			output: []string{
 				"CLUSTERROLEBINDING  SUBJECT  TYPE  SA-NAMESPACE",
 				"bob-can-get-logs    Bob      User"},
 		},
 		{
-			scenario: "Should print who can list services in the namespace `foo`",
-			args:     []string{"list", "services", "-n", "foo"},
+			name: "Should print who can list services in the namespace `foo`",
+			args: []string{"list", "services", "-n", "foo"},
 			output: []string{
 				"operator-can-view-services  foo        operator  ServiceAccount  bar",
 			},
 		},
 		{
-			scenario: "Should print who can scale deployments",
-			args:     []string{"update", "deployment", "--subresource", "scale"},
+			name: "Should print who can scale deployments",
+			args: []string{"update", "deployment", "--subresource", "scale"},
 			output: []string{
 				"devops-can-scale-workloads  default    devops   Group",
 			},
 		},
 		{
-			scenario: "Should print who can get pod named `pod-xyz` in the namespace `foo`",
-			args:     []string{"get", "pods/pod-xyz", "--namespace=foo"},
+			name: "Should print who can get pod named `pod-xyz` in the namespace `foo`",
+			args: []string{"get", "pods/pod-xyz", "--namespace=foo"},
 			output: []string{
 				"batman-can-view-pod-xyz  foo        Batman   User",
 			},
 		},
 		{
-			scenario: "Should print who can list pods in group `metrics.k8s.io`",
-			args:     []string{"list", "pods.metrics.k8s.io"},
+			name: "Should print who can list pods in group `metrics.k8s.io`",
+			args: []string{"list", "pods.metrics.k8s.io"},
 			output: []string{
-				"spiderman-can-view-pod-metrics                        Spiderman                           User",
+				"spiderman-can-view-pod-metrics               Spiderman                       User",
 			},
 		},
 	}
-	for _, tt := range data {
-		t.Run(tt.scenario, func(t *testing.T) {
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+
 			streams, _, out, _ := clioptions.NewTestIOStreams()
 			root, err := cmd.NewWhoCanCommand(streams)
 			require.NoError(t, err)
@@ -103,6 +113,12 @@ func TestIntegration(t *testing.T) {
 			prettyPrintWhoCanOutput(t, tt.args, out)
 
 			for _, line := range tt.output {
+				// TODO Improve asserts on the output
+				// I believe we can do better with such asserts by leveraging label selectors.
+				// By adding the "app.kubernetes.io/name=who-can" label to [Cluster]Roles and
+				// [Cluster]RoleBindings created by this integration test, we can distinguish
+				// them from the default RBAC settings which are labeled with
+				// "kubernetes.io/bootstrapping=rbac-defaults" or do not have labels at all.
 				assert.Contains(t, out.String(), line)
 			}
 		})
@@ -125,7 +141,8 @@ func createCRDs(t *testing.T, client clientext.CustomResourceDefinitionInterface
 	t.Helper()
 	_, err := client.Create(&apiext.CustomResourceDefinition{
 		ObjectMeta: meta.ObjectMeta{
-			Name: "pods.metrics.k8s.io",
+			Name:   "pods.metrics.k8s.io",
+			Labels: commonLabels,
 		},
 		Spec: apiext.CustomResourceDefinitionSpec{
 			Scope: apiext.NamespaceScoped,
@@ -155,9 +172,12 @@ func configureRBAC(t *testing.T, coreClient client.Interface) {
 
 	const namespaceFoo = "foo"
 
-	// Configure global namespace
+	// Define ClusterRoles and ClusterRoleBindings
 	_, err := clientRBAC.ClusterRoles().Create(&rbac.ClusterRole{
-		ObjectMeta: meta.ObjectMeta{Name: "create-configmaps"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "create-configmaps",
+			Labels: commonLabels,
+		},
 		Rules: []rbac.PolicyRule{
 			{
 				APIGroups: []string{""},
@@ -169,7 +189,10 @@ func configureRBAC(t *testing.T, coreClient client.Interface) {
 	require.NoError(t, err)
 
 	_, err = clientRBAC.ClusterRoles().Create(&rbac.ClusterRole{
-		ObjectMeta: meta.ObjectMeta{Name: "get-logs"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "get-logs",
+			Labels: commonLabels,
+		},
 		Rules: []rbac.PolicyRule{
 			{
 				Verbs:           []string{"get"},
@@ -180,7 +203,10 @@ func configureRBAC(t *testing.T, coreClient client.Interface) {
 	require.NoError(t, err)
 
 	_, err = clientRBAC.ClusterRoles().Create(&rbac.ClusterRole{
-		ObjectMeta: meta.ObjectMeta{Name: "view-pod-metrics"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "view-pod-metrics",
+			Labels: commonLabels,
+		},
 		Rules: []rbac.PolicyRule{
 			{
 				Verbs:     []string{"get", "list"},
@@ -191,31 +217,46 @@ func configureRBAC(t *testing.T, coreClient client.Interface) {
 	})
 
 	_, err = clientRBAC.ClusterRoleBindings().Create(&rbac.ClusterRoleBinding{
-		ObjectMeta: meta.ObjectMeta{Name: "bob-can-get-logs"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "bob-can-get-logs",
+			Labels: commonLabels,
+		},
 		RoleRef: rbac.RoleRef{
 			Name: "get-logs",
 			Kind: cmd.ClusterRoleKind,
 		},
 		Subjects: []rbac.Subject{
-			{Kind: rbac.UserKind, Name: "Bob"},
+			{
+				Kind: rbac.UserKind,
+				Name: "Bob",
+			},
 		},
 	})
 	require.NoError(t, err)
 
 	_, err = clientRBAC.ClusterRoleBindings().Create(&rbac.ClusterRoleBinding{
-		ObjectMeta: meta.ObjectMeta{Name: "spiderman-can-view-pod-metrics"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "spiderman-can-view-pod-metrics",
+			Labels: commonLabels,
+		},
 		RoleRef: rbac.RoleRef{
 			Name: "view-pod-metrics",
 			Kind: cmd.ClusterRoleKind,
 		},
 		Subjects: []rbac.Subject{
-			{Kind: rbac.UserKind, Name: "Spiderman"},
+			{
+				Kind: rbac.UserKind,
+				Name: "Spiderman",
+			},
 		},
 	})
 
-	// Configure default namespace
+	// Define Roles and RoleBindings
 	_, err = clientRBAC.Roles(core.NamespaceDefault).Create(&rbac.Role{
-		ObjectMeta: meta.ObjectMeta{Name: "create-configmaps"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "create-configmaps",
+			Labels: commonLabels,
+		},
 		Rules: []rbac.PolicyRule{
 			{
 				APIGroups: []string{""},
@@ -227,34 +268,49 @@ func configureRBAC(t *testing.T, coreClient client.Interface) {
 	require.NoError(t, err)
 
 	_, err = clientRBAC.RoleBindings(core.NamespaceDefault).Create(&rbac.RoleBinding{
-		ObjectMeta: meta.ObjectMeta{Name: "alice-can-create-configmaps"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "alice-can-create-configmaps",
+			Labels: commonLabels,
+		},
 		RoleRef: rbac.RoleRef{
 			Name: "create-configmaps",
 			Kind: cmd.RoleKind,
 		},
 		Subjects: []rbac.Subject{
-			{Kind: rbac.UserKind, Name: "Alice"},
+			{
+				Kind: rbac.UserKind,
+				Name: "Alice",
+			},
 		},
 	})
 	require.NoError(t, err)
 
 	_, err = clientRBAC.RoleBindings(core.NamespaceDefault).Create(&rbac.RoleBinding{
-		ObjectMeta: meta.ObjectMeta{Name: "rory-can-create-configmaps"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "rory-can-create-configmaps",
+			Labels: commonLabels,
+		},
 		RoleRef: rbac.RoleRef{
 			Name: "create-configmaps",
 			Kind: cmd.ClusterRoleKind,
 		},
 		Subjects: []rbac.Subject{
-			{Kind: rbac.UserKind, Name: "Rory"},
+			{
+				Kind: rbac.UserKind,
+				Name: "Rory",
+			},
 		},
 	})
 	require.NoError(t, err)
 
 	_, err = clientRBAC.Roles(core.NamespaceDefault).Create(&rbac.Role{
-		ObjectMeta: meta.ObjectMeta{Name: "scale-workloads"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "scale-workloads",
+			Labels: commonLabels,
+		},
 		Rules: []rbac.PolicyRule{
 			{
-				APIGroups: []string{"extensions"},
+				APIGroups: []string{"apps"}, // TODO For old clusters it's extensions, newer are apps
 				Verbs:     []string{"update"},
 				Resources: []string{"deployments/scale"},
 			},
@@ -262,24 +318,36 @@ func configureRBAC(t *testing.T, coreClient client.Interface) {
 	})
 
 	_, err = clientRBAC.RoleBindings(core.NamespaceDefault).Create(&rbac.RoleBinding{
-		ObjectMeta: meta.ObjectMeta{Name: "devops-can-scale-workloads"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "devops-can-scale-workloads",
+			Labels: commonLabels,
+		},
 		RoleRef: rbac.RoleRef{
 			Name: "scale-workloads",
 			Kind: cmd.RoleKind,
 		},
 		Subjects: []rbac.Subject{
-			{Kind: rbac.GroupKind, Name: "devops"},
+			{
+				Kind: rbac.GroupKind,
+				Name: "devops",
+			},
 		},
 	})
 
 	// Configure foo namespace
 	_, err = coreClient.CoreV1().Namespaces().Create(&core.Namespace{
-		ObjectMeta: meta.ObjectMeta{Name: namespaceFoo},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   namespaceFoo,
+			Labels: commonLabels,
+		},
 	})
 	require.NoError(t, err)
 
 	_, err = clientRBAC.Roles(namespaceFoo).Create(&rbac.Role{
-		ObjectMeta: meta.ObjectMeta{Name: "view-services"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "view-services",
+			Labels: commonLabels,
+		},
 		Rules: []rbac.PolicyRule{
 			{
 				APIGroups: []string{""},
@@ -296,7 +364,10 @@ func configureRBAC(t *testing.T, coreClient client.Interface) {
 	require.NoError(t, err)
 
 	_, err = clientRBAC.Roles(namespaceFoo).Create(&rbac.Role{
-		ObjectMeta: meta.ObjectMeta{Name: "view-pod-xyz"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "view-pod-xyz",
+			Labels: commonLabels,
+		},
 		Rules: []rbac.PolicyRule{
 			{
 				APIGroups:     []string{""},
@@ -308,24 +379,37 @@ func configureRBAC(t *testing.T, coreClient client.Interface) {
 	})
 
 	_, err = clientRBAC.RoleBindings(namespaceFoo).Create(&rbac.RoleBinding{
-		ObjectMeta: meta.ObjectMeta{Name: "operator-can-view-services"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "operator-can-view-services",
+			Labels: commonLabels,
+		},
 		RoleRef: rbac.RoleRef{
 			Name: "view-services",
 			Kind: cmd.RoleKind,
 		},
 		Subjects: []rbac.Subject{
-			{Kind: rbac.ServiceAccountKind, Name: "operator", Namespace: "bar"},
+			{
+				Kind:      rbac.ServiceAccountKind,
+				Name:      "operator",
+				Namespace: "bar",
+			},
 		},
 	})
 
 	_, err = clientRBAC.RoleBindings(namespaceFoo).Create(&rbac.RoleBinding{
-		ObjectMeta: meta.ObjectMeta{Name: "batman-can-view-pod-xyz"},
+		ObjectMeta: meta.ObjectMeta{
+			Name:   "batman-can-view-pod-xyz",
+			Labels: commonLabels,
+		},
 		RoleRef: rbac.RoleRef{
 			Name: "view-pod-xyz",
 			Kind: cmd.RoleKind,
 		},
 		Subjects: []rbac.Subject{
-			{Kind: rbac.UserKind, Name: "Batman"},
+			{
+				Kind: rbac.UserKind,
+				Name: "Batman",
+			},
 		},
 	})
 
