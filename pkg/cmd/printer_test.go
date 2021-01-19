91a35d2c450a368490eb8dcf329149c4eb2eedc9
Gituser143 <bhargavsnv100@gmail.com>
2021-01-19 14:28:32 +0530

    Add tests for ExportData in printer_test.go

--- pkg/cmd/printer_test.go
+++ pkg/cmd/printer_test.go
@@ -183,3 +183,121 @@ Bob-and-Eve-can-view-pods  ClusterRole/view  Eve      User
 	}
 
 }
+
+func TestPrinter_ExportData(t *testing.T) {
+	testCases := []struct {
+		scenario string
+
+		verb           string
+		resource       string
+		nonResourceURL string
+		resourceName   string
+
+		roleBindings        []rbac.RoleBinding
+		clusterRoleBindings []rbac.ClusterRoleBinding
+
+		wide   bool
+		output string
+	}{
+		{
+			scenario: "A",
+			verb:     "get", resource: "pods", resourceName: "",
+			output: "{}\n",
+		},
+		{
+			scenario: "B",
+			verb:     "get", resource: "pods", resourceName: "my-pod",
+			output: "{}\n",
+		},
+		{
+			scenario: "C",
+			verb:     "get", nonResourceURL: "/healthz",
+			output: "{}\n",
+		},
+		{
+			scenario: "D",
+			verb:     "get", resource: "pods",
+			roleBindings: []rbac.RoleBinding{
+				{
+					ObjectMeta: meta.ObjectMeta{Name: "Alice-can-view-pods", Namespace: "default"},
+					Subjects: []rbac.Subject{
+						{Name: "Alice", Kind: "User"},
+					}},
+				{
+					ObjectMeta: meta.ObjectMeta{Name: "Admins-can-view-pods", Namespace: "bar"},
+					Subjects: []rbac.Subject{
+						{Name: "Admins", Kind: "Group"},
+					}},
+			},
+			clusterRoleBindings: []rbac.ClusterRoleBinding{
+				{
+					ObjectMeta: meta.ObjectMeta{Name: "Bob-and-Eve-can-view-pods", Namespace: "default"},
+					Subjects: []rbac.Subject{
+						{Name: "Bob", Kind: "ServiceAccount", Namespace: "foo"},
+						{Name: "Eve", Kind: "User"},
+					},
+				},
+			},
+			output: "{\"clusterRoleBindings\":[{\"name\":\"Bob-and-Eve-can-view-pods\",\"roleRef\":{\"apiGroup\":\"\",\"kind\":\"\",\"name\":\"\"},\"subjects\":[{\"kind\":\"ServiceAccount\",\"name\":\"Bob\",\"namespace\":\"foo\"},{\"kind\":\"User\",\"name\":\"Eve\"}]}],\"roleBindings\":[{\"name\":\"Alice-can-view-pods\",\"roleRef\":{\"apiGroup\":\"\",\"kind\":\"\",\"name\":\"\"},\"subjects\":[{\"kind\":\"User\",\"name\":\"Alice\"}]},{\"name\":\"Admins-can-view-pods\",\"roleRef\":{\"apiGroup\":\"\",\"kind\":\"\",\"name\":\"\"},\"subjects\":[{\"kind\":\"Group\",\"name\":\"Admins\"}]}]}\n",
+		},
+		{
+			scenario: "E",
+			verb:     "get", resource: "pods",
+			roleBindings: []rbac.RoleBinding{
+				{
+					ObjectMeta: meta.ObjectMeta{Name: "Alice-can-view-pods", Namespace: "default"},
+					RoleRef: rbac.RoleRef{
+						Kind: cmd.RoleKind,
+						Name: "view-pods",
+					},
+					Subjects: []rbac.Subject{
+						{Name: "Alice", Kind: "User"},
+					}},
+				{
+					ObjectMeta: meta.ObjectMeta{Name: "Admins-can-view-pods", Namespace: "bar"},
+					RoleRef: rbac.RoleRef{
+						Kind: cmd.ClusterRoleKind,
+						Name: "view",
+					},
+					Subjects: []rbac.Subject{
+						{Name: "Admins", Kind: "Group"},
+					}},
+			},
+			clusterRoleBindings: []rbac.ClusterRoleBinding{
+				{
+					ObjectMeta: meta.ObjectMeta{Name: "Bob-and-Eve-can-view-pods", Namespace: "default"},
+					RoleRef: rbac.RoleRef{
+						Kind: cmd.ClusterRoleKind,
+						Name: "view",
+					},
+					Subjects: []rbac.Subject{
+						{Name: "Bob", Kind: "ServiceAccount", Namespace: "foo"},
+						{Name: "Eve", Kind: "User"},
+					},
+				},
+			},
+			wide: true,
+			output: "{\"clusterRoleBindings\":[{\"name\":\"Bob-and-Eve-can-view-pods\",\"roleRef\":{\"apiGroup\":\"\",\"kind\":\"ClusterRole\",\"name\":\"view\"},\"subjects\":[{\"kind\":\"ServiceAccount\",\"name\":\"Bob\",\"namespace\":\"foo\"},{\"kind\":\"User\",\"name\":\"Eve\"}]}],\"roleBindings\":[{\"name\":\"Alice-can-view-pods\",\"roleRef\":{\"apiGroup\":\"\",\"kind\":\"Role\",\"name\":\"view-pods\"},\"subjects\":[{\"kind\":\"User\",\"name\":\"Alice\"}]},{\"name\":\"Admins-can-view-pods\",\"roleRef\":{\"apiGroup\":\"\",\"kind\":\"ClusterRole\",\"name\":\"view\"},\"subjects\":[{\"kind\":\"Group\",\"name\":\"Admins\"}]}]}\n",
+		},
+	}
+
+	for _, tt := range testCases {
+		t.Run(tt.scenario, func(t *testing.T) {
+			// given
+			var buf bytes.Buffer
+			action := cmd.Action{
+				Verb:           tt.verb,
+				Resource:       tt.resource,
+				NonResourceURL: tt.nonResourceURL,
+				ResourceName:   tt.resourceName,
+			}
+
+			// when
+			cmd.NewPrinter(&buf, tt.wide).
+				ExportData(action, tt.roleBindings, tt.clusterRoleBindings)
+
+			// then
+			assert.Equal(t, tt.output, buf.String())
+		})
+	}
+}
